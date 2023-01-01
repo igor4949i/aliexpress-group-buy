5dc75faf3775369190e9a5b833f0adaa954251b7
igor4949i <igor4949i@gmail.com>
2023-01-01 17:58:35 +0200

    +

--- main.js
+++ main.js
@@ -37,3 +37,19 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+function copyT() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
