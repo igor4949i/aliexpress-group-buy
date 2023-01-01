2ec2e2ff28cdbca50f7d5b07bc81a7b6e052238d
igor4949i <igor4949i@gmail.com>
2023-01-01 15:53:02 +0200

    +

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.sort-table-product').DataTable({
+    "asSorting": [],
+    "scrollY": "100%",
+    "scrollX": true,
+
+    "scrollCollapse": true,
+    "paging": false
+  });
+  // $('.dataTables_length').addClass('bs-select');
+});
